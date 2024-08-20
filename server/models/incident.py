4ee5a321b0f80f574dba6cb4df596634cb8cea85
James Hawk <jph94880@gmail.com>
2024-08-19 21:56:56 -0500

    Adds incident shcema with constraints and validations in incident.py

--- server/models/incident.py
+++ server/models/incident.py
@@ -1,1 +1,125 @@
-print("I'm an incident.")
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from datetime import datetime
+
+from config import db
+from model_helpers import MAX_NAME_LENGTH, validate_model_input_string, MAX_INPUT_LENGTH
+
+# Validations To Do
+# incident_date, incident_time, incident_report_date, incident_report_time
+
+
+class Incident(db.Model, SerializerMixin):
+
+    __tablename__ = 'incidents'
+
+    id = db.Column(db.Integer, primary_key=True)
+    injury_description = db.Column(db.String(MAX_INPUT_LENGTH))
+    incident_date = db.Column(db.datetime, nullable=False)
+    incident_time = db.Column(db.datetime, nullable=False)
+    incident_report_date = db.Column(db.datetime, nullable=False)
+    incident_report_time = db.Column(db.datetime, nullable=False)
+    mechansim_of_injury = db.Column(db.String(MAX_INPUT_LENGTH), nullable=False)
+    body_part_injured = db.Column(db.String(MAX_INPUT_LENGTH), nullable=False)
+    symptoms = db.Column(db.String(MAX_INPUT_LENGTH), nullable=False)
+    incident_location = db.Column(db.String(MAX_NAME_LENGTH), nullable=False)
+    risk_assessment_severity = db.Column(db.Integer, nullable=False)
+    risk_assessment_probability = db.Column(db.Integer, nullable=False)
+    first_aid_given = db.Column(db.Boolean, nullable=False)
+    first_aid_details = db.Column(db.String(MAX_INPUT_LENGTH), nullable=False)
+    recovery_status = db.Column(db.String(MAX_INPUT_LENGTH), nullable=False)
+    pain_level = db.Column(db.Integer(2))
+    # Pain level constrant disallowing more than two characters. Validations will ensure users can only choose vlaues between 0 and 10. 
+
+    @validates('injury_description', 'mechanism_of_injury', 'body_part_injured', 'symptoms', 'incident_location', 'first_aid_details', 'recovery_status')
+    def validate_name(self, key, name):
+        """
+        Validates that the injury_description', 'mechanism_of_injury', 'body_part_injured', 'symptoms', 'incident_location', 'first_aid_details', 'recovery_status' attributes are all non-empty strings that are at most 260 characters long.
+
+        Args:
+            key (str): the attribute name.
+            name (str): the attribute value.
+
+        Returns:
+            str: the attribute value.
+        """
+        validate_model_input_string(key, name, MAX_INPUT_LENGTH)
+        return name
+    
+    @validates('risk_assessment_severity', 'risk_assessment_probability')
+    def validate_value(self, key, value):
+        if value is None:
+            raise ValueError(f'{key} must be between one and three')
+        if not isinstance(value, int):
+            return "value must be an integer"
+        if value > 3:
+            raise ValueError(f'{key} must be between one and three')
+        if value < 1:
+            raise ValueError(f'{key} must be between one and three')
+        return value
+    
+    @validates('pain_level')
+    def validate_pain_level(self, key, value):
+        """
+    Validates that pain_level is an integer between 0 and 10.
+
+    Args:
+        key (str): the attribute name.
+        value (int): the attribute value.
+
+    Returns:
+        int: the validated value.
+
+    Raises:
+        ValueError: If the value is outside the range 0-10.
+    """
+        if value is None:
+            raise ValueError(f'{key} cannot be null')
+        if not isinstance(value, int):
+            raise TypeError(f'{key} must be an integer')
+        if value < 0 or value > 10:
+            raise ValueError(f'{key} must be between zero and ten')
+        return value
+    
+    @validates('incident_date', 'incident_time', 'incident_report_date', 'incident_report_time')
+    def validate_datetime_fields(self, key, value):
+        """
+        Validates that datetime fields are not set in the future and that
+        the report datetime is after the incident datetime.
+
+        Args:
+            key (str): The attribute name being validated.
+            value (datetime): The value of the attribute.
+
+        Returns:
+            datetime: The validated value.
+
+        Raises:
+            ValueError: If the datetime is in the future or if the report datetime precedes the incident datetime.
+        """
+
+        if not isinstance(value, datetime):
+            raise TypeError(f'{key} must be a datetiem object')
+        now = datetime.now() 
+        if value > now: 
+            raise ValueError(f'{key} cannot be set in the future')
+        if key.startswith('incident_report') and hasattr(self, 'incident_date'):
+            incident_datetime = getattr(self, 'incident_date')
+            if value <= incident_datetime:
+                raise ValueError('Report datetime must be after incident datetime')
+        return value
+
+
+
+    
+    
+    
+
+
+
+
+    # Foreign Keys
+    # user_id
+    # employee_id
+    # medical records 
+    # company_id
